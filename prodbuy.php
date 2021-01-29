c1dfa928e5e5a9e58d591e2e11a7c774a6e9fbee
twicky99 <tharindu.wic@gmail.com>
2021-01-29 23:50:33 +0530

    updated prodbuy.php

--- prodbuy.php
+++ prodbuy.php
@@ -1,7 +1,7 @@
 <?php
 include("db.php");
 //create a variable called $pagename which contains the actual name of the page
-$pagename="“A smart buy for a smart home";
+$pagename="A smart buy for a smart home";
 
 //call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
@@ -12,9 +12,15 @@ echo "<title>".$pagename."</title>";
 include ("headfile.html");
 
 echo "<p></p>";
-//display name of the page and some random text
-echo "<h2>".$pagename."</h2>";
-echo "<p> Text Here";
+//display name of the web page
+echo "<h4>".$pagename."</h4>";
+
+//retrieve the product id passed from previous page using the GET method and the $_GET superglobal variable
+//applied to the query string u_prod_id
+//store the value in a local variable called $prodid
+$prodid=$_GET['u_prod_id'];
+//display the value of the product id, for debugging purposes
+echo "<p>Selected product Id: ".$prodid;
 
 //include head layout
 include("footfile.html");
