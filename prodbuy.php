19540300ea0bd2cb03990f2d6c458faaf3fd248a
twicky99 <tharindu.wic@gmail.com>
2021-01-30 00:15:04 +0530

    added no.of items displaying message and few other chnages are done

--- prodbuy.php
+++ prodbuy.php
@@ -22,6 +22,37 @@ $prodid=$_GET['u_prod_id'];
 //display the value of the product id, for debugging purposes
 echo "<p>Selected product Id: ".$prodid;
 
+//create a $SQL variable and populate it with a SQL statement that retrieves product details
+$SQL="select prodId, prodName,prodDescripLong,prodPrice, prodPicNameLarge ,prodQuantity from Product WHERE prodId=$prodid";
+//run SQL query for connected DB or exit and display error message
+$exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error());
+echo "<table style='border: 0px'>";
+//create an array of records (2 dimensional variable) called $arrayp.
+//populate it with the records retrieved by the SQL query previously executed.
+//Iterate through the array i.e while the end of the array has not been reached, run through it
+while ($arrayp=mysqli_fetch_array($exeSQL))
+{
+    echo "<tr>";
+    echo "<td style='border: 0px'>";
+
+    //make the image into an anchor to prodbuy.php and pass the product id by URL (the id from the array)
+    echo "<a href=prodbuy.php?u_prod_id=".$arrayp['prodId'].">";
+    //display the small image whose name is contained in the array
+    echo "<img src=images/".$arrayp['prodPicNameLarge']." height=200 width=200>";
+    //close the anchor
+    echo"</a>";
+    
+    echo "</td>";
+    echo "<td style='border: 0px'>";
+    echo "<p><h5>".$arrayp['prodName']."</h5>"; //display product name as contained in the array
+    echo "<p>".$arrayp['prodDescripLong']."</p>"; //display product short description as contained in the array
+    echo "<p><b>&pound;".$arrayp['prodPrice']."</b></p>"; //display product price as contained in the array
+    echo "<p> Number of items left in stock: ".$arrayp['prodQuantity']."</p>"; //display no.of items left in the stock
+    echo "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
 //include foot layout
 include("footfile.html");
 ?>
